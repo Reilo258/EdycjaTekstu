e18eb394e7c60c2e8ace904f1c329a32e138e7e0
Reilo258 <olivier258@wp.pl>
2021-11-17 08:42:57 +0100

    Done!

--- app/src/main/java/com/example/edycjatekstu/MainActivity.kt
+++ app/src/main/java/com/example/edycjatekstu/MainActivity.kt
@@ -10,8 +10,12 @@ import android.widget.EditText
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btnglowny).setOnClickListener {
-            findViewById<EditText>(R.id.multilinia).text;
+        findViewById<Button>(R.id.btnimie).setOnClickListener {
+            findViewById<EditText>(R.id.multilinia).setText(R.string.MultiLinia);
+        }
+
+        findViewById<Button>(R.id.btn2).setOnClickListener {
+            findViewById<EditText>(R.id.jednalinia).setText(R.string.PojedynczaLinia);
         }
     }
 }
